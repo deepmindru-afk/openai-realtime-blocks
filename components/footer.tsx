7045b095d521106ec63ec1e307d653e65e726ab0
deepmindru-afk <deepmindru@gmail.com>
2025-10-13 03:56:25 +0300

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,7 +8,7 @@ export function Footer() {
       <div className="max-md:px-4 md:container h-12 flex justify-start items-center">
         <div className="flex gap-3 items-center justify-end">
           <Link
-            href={siteConfig.links.twitter}
+            href="https://www.portalos.ru"
             target="_blank"
             rel="noreferrer noopener"
           >
@@ -17,8 +17,8 @@ export function Footer() {
               size="sm"
               className="flex gap-1 items-center px-0 text-muted-foreground hover:text-foreground hover:no-underline"
             >
-              #BuildingInPublic{" "}
-              <span className="font-semibold">@cameronking4</span>
+              #АО{" "}
+              <span className="font-semibold">Портал</span>
             </Button>
           </Link>
         </div>
