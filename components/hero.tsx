91bdb21a7f7e60a0458b751492f87e56fae1517f
deepmindru-afk <deepmindru@gmail.com>
2025-10-13 03:58:27 +0300

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -8,7 +8,7 @@ export default function Hero() {
       <div className="flex flex-col justify-center space-y-4 text-center">
         <div className="space-y-2">
           <span className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl/none text-transparent bg-clip-text bg-gradient-to-r from-white to-white/30">
-            Your next component starts here
+            Ваш ИИ — тут
           </span>
           <p className="w-full px-10 md:text-xl lg:text-lg xl:text-xl text-transparent bg-clip-text bg-gradient-to-r from-white to-white/30">
             {siteConfig.description}
@@ -16,13 +16,13 @@ export default function Hero() {
         </div>
         <div className="flex gap-2 justify-center">
           <a
-            href="https://github.com/cameronking4/AI Blocks"
+            href="https://www.portalos.ru"
             className="no-underline"
             target="_blank"
             rel="noopener noreferrer"
           >
             <Button variant="outline" className="flex items-center gap-2">
-              Star on GitHub <Icons.github />
+              Star <Icons.github />
             </Button>
           </a>
         </div>
